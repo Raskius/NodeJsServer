984152d66af3072482823f489a83eec4eca62591
Raskius <bruno.m.cruz@hotmail.com>
2024-01-15 02:16:11 +0000

    Change directory structure

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
-import Item from './item.js';
+import itemController from './src/controller/itemController.js'
 
 const app = express();
 const port = 3000;
@@ -8,57 +7,8 @@ const port = 3000;
 // Middleware to parse JSON requests
 app.use(express.json());
 
-// Example data (in-memory storage for demonstration purposes)
-const item1 = new Item('Item 1')
-const item2 = new Item('Item 2')
-const item3 = new Item('Item 3')
-let items = [item1, item2, item3];
-
-// Get all items
-app.get('/items', (req, res) => {
-  res.json(items);
-});
-
-// Get a specific item by ID
-app.get('/items/:id', (req, res) => {
-  const itemId = parseInt(req.params.id);
-  const item = items.find((item) => item.id === itemId);
-
-  if (item) {
-    res.json(item);
-  } else {
-    res.status(404).json({ message: 'Item not found' });
-  }
-});
-
-// Create a new item
-app.post('/items', (req, res) => {
-  const hasItem = Item.containsName(items, req.body)
-
-  if (hasItem) {
-    res.status(400).json({ error: `There is already an item with name: \'${req.body.name}\'` })
-    return;
-  }
-
-  const newItem = new Item(req.body.name);
-  items.push(newItem)
-  res.status(201).json({ message: `Created request ${newItem}!` });
-})
-
-
-// Delete the item given the ID
-app.delete('/items/:id', (req, res) => {
-  const itemIndex = items.findIndex(i => i.id == req.params.id);
-  
-  if (itemIndex == -1) {
-    res.status(400).json({ error: `Could not find an item with id: '${req.params.id}'` });
-    return;
-  }
-
-  items.splice(itemIndex, 1);
-  res.status(200).json({ message: `Item '${req.params.id}' successfully deleted.` });
-});
-
+// Use the itemsController to handle item-related requests
+app.use('/', itemController);
 
 // Start the server
 app.listen(port, () => {
